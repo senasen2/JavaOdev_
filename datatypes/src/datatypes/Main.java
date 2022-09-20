3e8e78fac3f729d7bf4a2a014c880f662f52eca1
SENA ŞEN <senasen2111@gmail.com>
2022-09-20 21:51:03 +0300

    DataTypes

---
+++ datatypes/src/datatypes/Main.java
@@ -0,0 +1,18 @@
+package datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		double sayi =12.5; 
+		sayi = -129;
+		
+		String sehir ="İstanbul";
+		char karakter = "S";
+		
+		boolean dogruMu=false;
+		
+
+	}
+
+}
